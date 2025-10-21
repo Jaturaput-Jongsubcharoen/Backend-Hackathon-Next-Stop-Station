10c01e8527b14b09e24a9c487f079177abeaed1d
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-10-21 09:52:50 -0400

    fix cors in server.js add fallback for Render and localhost support

--- server.js
+++ server.js
@@ -4,34 +4,28 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 8081;
-const URL_FRONTEND = process.env.URL_FRONTEND;
+const URL_FRONTEND =
+  process.env.URL_FRONTEND || "https://frontend-hackathon-next-stop-station.onrender.com";
 
-// Import fleet data from local file
 const fleetData = require("./data/fleetData");
 
-// Middleware
 app.use(express.json());
+
 app.use(
   cors({
-    origin: URL_FRONTEND,
+    origin: [URL_FRONTEND, "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-
-// API Routes
-// -------------------------------------
-// Fleet data route
 app.get("/api/battery", (req, res) => {
   res.json(fleetData);
 });
 
-// untouched
 app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "orange", "banana"] });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
