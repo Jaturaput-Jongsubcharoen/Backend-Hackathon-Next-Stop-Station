f31b2277fcd2530891ca457e92058701e1a0f323
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-10-21 09:09:54 -0400

    create backend Real-time Predictive Maintenance System

--- server.js
+++ server.js
@@ -4,21 +4,34 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 8081;
-const URL_FRONTEND = process.env.URL_FRONTEND || "http://localhost:5173";
+const URL_FRONTEND = process.env.URL_FRONTEND;
 
+// Import fleet data from local file
+const fleetData = require("./data/fleetData");
+
+// Middleware
 app.use(express.json());
-
 app.use(
   cors({
     origin: URL_FRONTEND,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // IMPORTANT
+    credentials: true,
   })
 );
 
+
+// API Routes
+// -------------------------------------
+// Fleet data route
+app.get("/api/battery", (req, res) => {
+  res.json(fleetData);
+});
+
+// untouched
 app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "orange", "banana"] });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
